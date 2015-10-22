5d2c3e7a9ae31161051300aeb4ac23994d28d0b9
Mateusz Korszun <mkorszun@gmail.com>
2015-10-22 19:44:26 +0200

    Pace is optional

--- src/main/scala/model/event/Event.scala
+++ src/main/scala/model/event/Event.scala
@@ -1,4 +1,4 @@
 package model.event
 
 case class Event(headline: String, description: Option[String], timestamp: Long, x: Double, y: Double,
-    tags: Option[Array[String]], distance: Int, pace: Double)
+    tags: Option[Array[String]], distance: Int, pace: Option[Double])
