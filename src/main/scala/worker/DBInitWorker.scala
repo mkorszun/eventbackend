81ec9579cd93f195027c222bbbc4d42bab326d1f
Mateusz Korszun <mkorszun@gmail.com>
2015-12-07 16:27:08 +0100

    Update indexes

--- src/main/scala/worker/DBInitWorker.scala
+++ src/main/scala/worker/DBInitWorker.scala
@@ -6,19 +6,27 @@ import com.mongodb.casbah.{Imports, MongoClient, MongoClientURI, MongoDB}
 
 object DBInitWorker {
 
+    val UNIQUE = MongoDBObject("unique" -> true)
     val collections: Array[String] = Array("events", "users")
 
     val event_indexes: Array[Imports.DBObject] = Array(
         MongoDBObject("participants.id" -> 1, "timestamp" -> 1),
         MongoDBObject("loc" -> "2dsphere", "timestamp" -> 1, "tags" -> 1),
-        MongoDBObject("participants.id" -> 1, "user.id" -> 1),
+        MongoDBObject("participants.id" -> 1, "_id" -> 1),
         MongoDBObject("_id" -> 1, "user.id" -> 1)
     )
 
     val user_indexes: Array[Imports.DBObject] = Array(
         MongoDBObject("provider" -> 1, "provider_id" -> 1),
-        MongoDBObject("_id" -> 1, "token" -> 1)
+        MongoDBObject("_id" -> 1, "token" -> 1),
+        MongoDBObject("email" -> 1, "verified" -> 1),
+        MongoDBObject("token" -> 1, "verified" -> 1)
     )
+
+    val unique_user_indexes: Array[Imports.DBObject] = Array(
+        MongoDBObject("email" -> 1)
+    )
+
     val db = getDB()
 
     def getDB(): MongoDB = {
@@ -33,12 +41,14 @@ object DBInitWorker {
         val users: DBCollection = db.getCollection("users")
         val events: DBCollection = db.getCollection("events")
 
-        println("Creating indexes")
         for (collection <- collections) {
             if (!db.collectionExists(collection)) db.createCollection(collection, MongoDBObject())
         }
+
+        println("Creating indexes")
         for (idx <- user_indexes) users.createIndex(idx)
         for (idx <- event_indexes) events.createIndex(idx)
+        for (idx <- unique_user_indexes) users.createIndex(idx, UNIQUE)
     }
 
     def main(args: Array[String]): Unit = {
