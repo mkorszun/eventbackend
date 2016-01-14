c5bfdb012cc1b084f0336b43b16c83eed121bc56
Mateusz Korszun <mkorszun@gmail.com>
2016-01-14 20:59:46 +0100

    Pace is double in admin event

--- src/main/scala/model/admin/AdminEvent.scala
+++ src/main/scala/model/admin/AdminEvent.scala
@@ -3,4 +3,4 @@ package model.admin
 import model.user.PublicUser
 
 case class AdminEvent(headline: String, description: Option[String], timestamp: Long, x: Double, y: Double,
-    tags: Option[Array[String]], distance: Int, pace: Option[Double], user: PublicUser, www: Option[String])
+    tags: Option[Array[String]], distance: Option[Double], pace: Option[Double], user: PublicUser, www: Option[String])
