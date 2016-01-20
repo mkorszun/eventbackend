e1744ffcea96531a23a8acf75c0e1d71382bedf9
Mateusz Korszun <mkorszun@gmail.com>
2016-01-20 11:58:54 +0100

    Accept settings as integers

--- src/main/scala/format/PublicUserFormat.scala
+++ src/main/scala/format/PublicUserFormat.scala
@@ -1,9 +1,25 @@
 package format
 
 import model.user.{PublicUser, UserDeviceSettings}
-import spray.json.DefaultJsonProtocol
+import spray.json._
 
 object PublicUserJsonProtocol extends DefaultJsonProtocol {
+
+    implicit object MyBooleanJsonFormat extends JsonFormat[Boolean] {
+        def write(value: Boolean): JsBoolean = {
+            return JsBoolean(value)
+        }
+
+        def read(value: JsValue) = {
+            value match {
+                case JsNumber(n) if n == 1 => true
+                case JsNumber(n) if n == 0 => false
+                case JsBoolean(v) => v
+                case _ => throw new DeserializationException("Not a boolean")
+            }
+        }
+    }
+
     implicit val userDeviceSettingsFormat = jsonFormat4(UserDeviceSettings.apply)
     implicit val publicUserFormat = jsonFormat9(PublicUser.apply)
 }
