6cc25991ac2a227b261b717987dfe0dddcbea5d3
Mateusz Korszun <mkorszun@gmail.com>
2016-01-28 15:41:11 +0100

    Distance is optional

--- src/main/scala/model/event/Event.scala
+++ src/main/scala/model/event/Event.scala
@@ -1,4 +1,4 @@
 package model.event
 
 case class Event(headline: String, description: Option[String], timestamp: Long, x: Double, y: Double,
-    tags: Option[Array[String]], distance: Double, pace: Option[Double])
+    tags: Option[Array[String]], distance: Option[Double], pace: Option[Double])
